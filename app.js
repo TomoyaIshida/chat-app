b6b7f576ceabf0a7ecba21503e067dc5ce586276
TomoyaIshida <ishidatomoya@ishidatomoya-no-MacBook-Pro.local>
2017-08-05 22:32:19 +0900

    message-function

--- app.js
+++ app.js
@@ -1,10 +1,30 @@
-var app = require('express')();
-var http = require('http').Server(app);
+var fs = require("fs");
+var server = require("http").createServer(function(req, res) {
+     res.writeHead(200, {"Content-Type":"text/html"});
+     var output = fs.readFileSync("./index.html", "utf-8");
+     res.end(output);
+}).listen(8080);
+var io = require("socket.io").listen(server);
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
-});
+var userHash = {};
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+io.sockets.on("connection", function (socket) {
+
+  socket.on("connected", function (name) {
+    var msg = name + "が入室しました";
+    userHash[socket.id] = name;
+    io.sockets.emit("publish", {value: msg});
+  });
+
+  socket.on("publish", function (data) {
+    io.sockets.emit("publish", {value:data.value});
+  });
+
+  socket.on("disconnect", function () {
+    if (userHash[socket.id]) {
+      var msg = userHash[socket.id] + "が退出しました";
+      delete userHash[socket.id];
+      io.sockets.emit("publish", {value: msg});
+    }
+  });
 });
